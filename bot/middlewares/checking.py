c925d7488367bc43d78049b8b54604e6a5d02b5e
ChoriyevJamshid <qpq777111000@gmail.com>
2025-08-07 14:47:54 +0500

    .

--- bot/middlewares/checking.py
+++ bot/middlewares/checking.py
@@ -17,12 +17,18 @@ class CheckingMiddleware(BaseMiddleware):
 
         if not event.message and not event.callback_query:
             return await handler(event, data)
+
         if event.message:
             tg_user = event.message.from_user
+            call_data = None
         else:
             tg_user = event.callback_query.from_user
+            call_data = event.callback_query.data
 
         if tg_user.is_bot:
+            return await handler(event, data)
+
+        if call_data and call_data.startswith('group-ready'):
             return await handler(event, data)
 
         user = await utils.get_user(tg_user)
