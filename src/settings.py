b6029120000024a293ba4edc4cd1ed418bc9bb77
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-03-09 17:28:05 +0500

    .

--- src/settings.py
+++ src/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 if not DEBUG:
     STORAGES = {
